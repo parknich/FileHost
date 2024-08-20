895492ce5109716d829f15c3ea2874cab58757c9
parknich081 <parknich082@gmail.com>
2024-08-20 16:58:58 -0400

    import validator oops

--- file-host-backend/routes/auth.js
+++ file-host-backend/routes/auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
+const validator = require('validator');
 
 const router = express.Router();
 
